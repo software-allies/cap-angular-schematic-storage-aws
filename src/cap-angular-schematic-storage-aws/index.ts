f11ad5a052197e72e2258ca09c486746ade6977f
Diego SanMo <diego14290764@itcg.edu.mx>
2020-01-20 16:54:56 -0600

    Functions to add the npm dependencies into the package JSON were added.

--- src/cap-angular-schematic-storage-aws/index.ts
+++ src/cap-angular-schematic-storage-aws/index.ts
@@ -1,6 +1,29 @@
-import { apply, MergeStrategy, mergeWith, Rule, move, SchematicContext, Tree, template, url } from '@angular-devkit/schematics';
-import { join, normalize } from 'path';
-import { getWorkspace } from '@schematics/angular/utility/config';
+import { apply, MergeStrategy, mergeWith, Rule, move, SchematicContext, Tree, template, url, chain, noop } from '@angular-devkit/schematics';
+// import { join, normalize } from 'path';
+// import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+
+// import { getWorkspace } from '@schematics/angular/utility/config';
+// import {
+//   addModuleImportToRootModule,
+//   addPackageJsonDependency,
+//   getProjectFromWorkspace,
+//   getWorkspace,
+//   NodeDependency,
+//   NodeDependencyType
+// } from 'schematics-utilities';
+
+// import { , , , ,  } from '@angular-devkit/schematics';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+import {
+  addModuleImportToRootModule,
+  addPackageJsonDependency,
+  getProjectFromWorkspace,
+  getWorkspace,
+  NodeDependency,
+  NodeDependencyType
+} from 'schematics-utilities';
+import { normalize, join } from 'path';
+
 
 export function setupOptions(host: Tree, options: any): Tree {
   const workspace = getWorkspace(host);
@@ -23,8 +46,63 @@ export function capAngularSchematicStorageAws(_options: any): Rule {
       template({ ..._options }),
       move(movePath)
     ]);
-    console.log('templateSource: ', templateSource);
     const rule = mergeWith(templateSource, MergeStrategy.Overwrite);
     return rule(tree, _context);
   };
 }
+
+
+export function addPackageJsonDependencies(): Rule {
+  return (host: Tree, context: SchematicContext) => {
+    const dependencies: NodeDependency[] = [
+      { type: NodeDependencyType.Default, version: '~2.606.0', name: 'aws-sdk' },
+      { type: NodeDependencyType.Default, version: '~8.0.8', name: 'ngx-file-drop' },
+      // { type: NodeDependencyType.Default, version: '~1.1.0', name: 'angular-made-with-love' }
+    ];
+
+    dependencies.forEach(dependency => {
+      addPackageJsonDependency(host, dependency);
+      context.logger.log('info', `✅️ Added "${dependency.name}" into ${dependency.type}`);
+    });
+
+    return host;
+  };
+}
+
+export function installPackageJsonDependencies(): Rule {
+  return (host: Tree, context: SchematicContext) => {
+    context.addTask(new NodePackageInstallTask());
+    context.logger.log('info', `🔍 Installing packages...`);
+
+    return host;
+  };
+}
+
+function addModuleToImports(options: any): Rule {
+  return (host: Tree, context: SchematicContext) => {
+    const workspace = getWorkspace(host);
+    const project = getProjectFromWorkspace(
+      workspace,
+      // Takes the first project in case it's not provided by CLI
+      options.project ? options.project : Object.keys(workspace['projects'])[0]
+      );
+    const moduleName = 'CapStorageAWS';
+
+    addModuleImportToRootModule(host, moduleName, 'AppModule', project);
+    context.logger.log('info', `✅️ "${moduleName}" is imported`);
+
+    return host;
+  };
+}
+
+export default function (options: any): Rule {
+  console.log('options: ', options);
+  return chain([
+    options && options.skipModuleImport ? noop() : capAngularSchematicStorageAws(options),
+    options && options.skipPackageJson ? noop() : addPackageJsonDependencies(),
+    options && options.skipPackageJson ? noop() : installPackageJsonDependencies(),
+    options && options.skipModuleImport ? noop() : addModuleToImports(options),
+    // options && options.skipPolyfill ? noop() : addPolyfillToScripts(options)
+  ]);
+}
+
