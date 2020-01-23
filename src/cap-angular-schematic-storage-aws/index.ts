c893075e18c058f6f88a05de25463ab218c6d4b3
Diego SanMo <diego14290764@itcg.edu.mx>
2020-01-23 16:28:27 -0600

    The function addToRootModule was added to check changes into the root module and added the CapAWSModule.

--- src/cap-angular-schematic-storage-aws/index.ts
+++ src/cap-angular-schematic-storage-aws/index.ts
@@ -1,29 +1,16 @@
-import { apply, MergeStrategy, mergeWith, Rule, move, SchematicContext, Tree, template, url, chain, noop } from '@angular-devkit/schematics';
-// import { join, normalize } from 'path';
-// import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+import { apply, MergeStrategy, mergeWith, Rule, move, SchematicContext, Tree, template, url, chain, noop, forEach, FileEntry, SchematicsException } from '@angular-devkit/schematics';
+import { getWorkspace, getAppModulePath, addImportToModule } from 'schematics-utilities';
 
-// import { getWorkspace } from '@schematics/angular/utility/config';
-// import {
-//   addModuleImportToRootModule,
-//   addPackageJsonDependency,
-//   getProjectFromWorkspace,
-//   getWorkspace,
-//   NodeDependency,
-//   NodeDependencyType
-// } from 'schematics-utilities';
-
-// import { , , , ,  } from '@angular-devkit/schematics';
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 import {
-  addModuleImportToRootModule,
   addPackageJsonDependency,
   getProjectFromWorkspace,
-  getWorkspace,
   NodeDependency,
-  NodeDependencyType
+  NodeDependencyType,
 } from 'schematics-utilities';
-import { normalize, join } from 'path';
-
+import { getProjectMainFile, getSourceFile } from 'schematics-utilities/dist/cdk';
+import { normalize, join } from '@angular-devkit/core';
+import { InsertChange } from '@schematics/angular/utility/change';
 
 export function setupOptions(host: Tree, options: any): Tree {
   const workspace = getWorkspace(host);
@@ -36,15 +23,21 @@ export function setupOptions(host: Tree, options: any): Tree {
   return host;
 }
 
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
 export function capAngularSchematicStorageAws(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
     setupOptions(tree, _options);
     const movePath = normalize(_options.path + '/');
     const templateSource = apply(url('./files'), [
-      template({ ..._options }),
-      move(movePath)
+      template({
+        ..._options
+      }),
+      move(movePath),
+      forEach((fileEntry: FileEntry) => {
+        if (tree.exists(fileEntry.path)) {
+          tree.overwrite(fileEntry.path, fileEntry.content);
+        }
+        return fileEntry;
+      }),
     ]);
     const rule = mergeWith(templateSource, MergeStrategy.Overwrite);
     return rule(tree, _context);
@@ -57,7 +50,7 @@ export function addPackageJsonDependencies(): Rule {
     const dependencies: NodeDependency[] = [
       { type: NodeDependencyType.Default, version: '~2.606.0', name: 'aws-sdk' },
       { type: NodeDependencyType.Default, version: '~8.0.8', name: 'ngx-file-drop' },
-      // { type: NodeDependencyType.Default, version: '~1.1.0', name: 'angular-made-with-love' }
+      { type: NodeDependencyType.Default, version: '~13.5.0', name: '@types/node' },
     ];
 
     dependencies.forEach(dependency => {
@@ -79,30 +72,56 @@ export function installPackageJsonDependencies(): Rule {
 }
 
 function addModuleToImports(options: any): Rule {
-  return (host: Tree, context: SchematicContext) => {
+  return (host: Tree) => {
     const workspace = getWorkspace(host);
     const project = getProjectFromWorkspace(
       workspace,
       // Takes the first project in case it's not provided by CLI
       options.project ? options.project : Object.keys(workspace['projects'])[0]
-      );
+    );
+    const modulePath = getAppModulePath(host, getProjectMainFile(project));
     const moduleName = 'CapStorageAWS';
-
-    addModuleImportToRootModule(host, moduleName, 'AppModule', project);
-    context.logger.log('info', `✅️ "${moduleName}" is imported`);
-
+    addToRootModule(host, modulePath, moduleName, './modules/cap-storage-aws/cap-storage.module', options)
     return host;
   };
 }
 
 export default function (options: any): Rule {
-  console.log('options: ', options);
   return chain([
     options && options.skipModuleImport ? noop() : capAngularSchematicStorageAws(options),
     options && options.skipPackageJson ? noop() : addPackageJsonDependencies(),
     options && options.skipPackageJson ? noop() : installPackageJsonDependencies(),
     options && options.skipModuleImport ? noop() : addModuleToImports(options),
-    // options && options.skipPolyfill ? noop() : addPolyfillToScripts(options)
   ]);
 }
 
+export function addToRootModule(host: Tree, modulePath: string, moduleName: string, src: string,  options?:any) {
+
+  const moduleSource = getSourceFile(host, modulePath);
+
+  if (!moduleSource) {
+    throw new SchematicsException(`Module not found: ${modulePath}`);
+  }
+
+  const changes = addImportToModule(moduleSource as any, modulePath, moduleName, src);
+  let recorder = host.beginUpdate(modulePath);
+
+  changes.forEach((change: any) => {
+    if (change instanceof InsertChange) {
+      if (change.toAdd === ',\n    CapStorageAWS') {
+        change.toAdd = `,\n    CapStorageAWS.forRoot({
+        bucket: '${options.bucket}',
+        accessKeyId: '${options.accessKeyId}',
+        secretAccessKey: '${options.secretAccessKey}',
+        region: '${options.region}',
+        folder: '${options.folder}'
+        })`;
+      }
+      recorder.insertLeft(change.pos, change.toAdd);
+
+    }
+  });
+  host.commitUpdate(recorder);
+
+  return host
+}
